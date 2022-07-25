ad32b45e9d082c8154083f7e6dfc72169894843d
Ivan <ivan.p@externals.adidas.com>
2022-07-25 12:30:29 +0300

    exp page

--- src/scripts/sections/experience.js
+++ src/scripts/sections/experience.js
@@ -31,17 +31,19 @@ export function init(resources) {
  * }} data 
  */
 function drawSvg(data) {
+    const positionsArr = data.positions.map(x => x);
     const mainCircleSizeR = 10;
 
     const svgContainer = d3.select(`.${data.company}`);
     /**
      * @type {DOMRect}
      */
-    const {width, height} = svgContainer.node().getBoundingClientRect();
+    const {width, height, x, y} = svgContainer.node().getBoundingClientRect();
 
     const linePosition = {
-        x1: '20%',
-        y1: '10%',
+        x1: 100,
+        y1: 100,
+
         y2: height - 10 
     }
 
@@ -67,7 +69,7 @@ function drawSvg(data) {
 
         const subCircles = lineG
         .selectAll('circles')
-        .data(data.positions.map(x => x))
+        .data(positionsArr)
         .enter()
         .append('circle')
         .attr('r', mainCircleSizeR / 2)
@@ -85,10 +87,82 @@ function drawSvg(data) {
         .attr('class','company_title_g')
         .append('text')
         .attr('class', 'company_title')
-        .attr('x', linePosition.x1)
-        .attr('y', linePosition.y1)
-        .text(data.company);
+        .attr('x', linePosition.x1 + 50)
+        .text(data.company)
 
+        .attr('y', function() {
+            const {height} = this.getBoundingClientRect();
+            return linePosition.y1 + height / 4;
+        });
+
+
+        const subItemsG = svgContainer.append('g');
+
+        
+        const itemH = 176;
+
+        const rects = subItemsG
+        .selectAll('rects')
+        .data(positionsArr)
+        .enter()
+        .append('rect')
+        .attr('x', linePosition.x1)
+        .attr('y', function(d, i) {
+            return subCirclesPositions[i] - 20;
+        })
+        .attr('width', width - linePosition.x1 - 2)
+        .attr('height', itemH)
+        //.attr('stroke', '#dadada')
+        //.attr('stroke-width', 2)
+        .attr('fill', 'transparent');
+        
+        subItemsG
+        .selectAll('sub-text')
+        .data(positionsArr)
+        .enter()
+        .append('text')
+        .attr('class', 'position_title')
+        .attr('x', linePosition.x1 + 50)
+        .text(d => d.name)
+        .attr('y', function(d, i) {
+            const {height} = this.getBoundingClientRect();
+            d.mainTextY = subCirclesPositions[i] + height / 4;
+            return d.mainTextY;
+        });
+
+        const newG = svgContainer.insert('g', ':first-child');
+        const fOb = newG.selectAll('sub-text-desc')
+        .data(positionsArr)
+        .enter()
+        .append('foreignObject')
+        .attr('class', 'desc-text')
+        .attr('x', linePosition.x1 + 50)
+        .attr('y', d => d.mainTextY + 30)
+        .attr('width', width - linePosition.x1 - 50)
+        .attr('height', itemH);
+
+        fOb
+        .html((d) => `<div>${d.shortDescription}</div>`);
+       
+
+
+        rects.on('mouseover', function() {
+            const thisRect = d3.select(this);
+            thisRect.transition()
+            .duration(1000)
+            .attr('fill', '#dadada')
+            .attr('color', 'white')
+
+
+
+        })
+        .on('mouseout', function() {
+            const thisRect = d3.select(this);
+            thisRect.transition()
+            .duration(1000)
+            .attr('fill', 'transparent')
+        })
+        
 }
 
 
